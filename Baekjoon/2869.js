b3db488db7974497aa70d8826619702a0c5adb2a
jyi3479 <jyi3479@naver.com>
2022-03-03 03:39:48 +0900

    2869번 수정

--- Baekjoon/2869.js
+++ Baekjoon/2869.js
@@ -3,15 +3,6 @@ var a = parseInt(input[0]);
 var b = parseInt(input[1]);
 var c = parseInt(input[2]);
 
-console.log(a, b, c);
-
-var cnt = 0;
-
-while (c > 0) {
-  cnt++;
-  c = c - a;
-  if (c > 0) {
-    c = c + b;
-  }
-}
-console.log(cnt);
+// 하루 동안 올라간 높이 a-b
+// 마지막 날 도달해야 하는 높이 c-b
+console.log(Math.ceil((c - b) / (a - b)));
--- index.js
+++ index.js
@@ -3,15 +3,19 @@ var a = parseInt(input[0]);
 var b = parseInt(input[1]);
 var c = parseInt(input[2]);
 
-console.log(a, b, c);
+// 하루 동안 올라간 높이 a-b
+// 마지막 날 도달해야 하는 높이 c-b
+console.log(Math.ceil((c - b) / (a - b)));
 
-var cnt = 0;
+// console.log(a, b, c);
 
-while (c > 0) {
-  cnt++;
-  c = c - a;
-  if (c > 0) {
-    c = c + b;
-  }
-}
-console.log(cnt);
+// var cnt = 1;
+// var temp = c - a;
+
+// // 밤이 되기 전 남은 높이가 0보다 큰 지 확인하기
+// while (temp > 0) {
+//   cnt++;
+//   temp += b - a;
+// }
+
+// console.log(cnt);
